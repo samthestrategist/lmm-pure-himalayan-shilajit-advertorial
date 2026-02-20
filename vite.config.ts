0629d38fbce32f5539801c244b383277ea91c0e2
samthestrategist <sam@littlemissmineral.com>
2026-02-20 07:16:51 -0800

    Fix: Correct vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/lmm-advertorial-golden-template/",
+  base: "/lmm-pure-himalayan-shilajit-advertorial/",
   plugins: [react()],
   resolve: {
     alias: {
